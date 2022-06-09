ffa944886ce76f546be01b3a62b902cb8f24e718
Stephane Plaisance <stephane.plaisance@vib.be>
2022-06-09 13:13:59 +0200

    Update conda_backup_envs.sh

--- conda_backup_envs.sh
+++ conda_backup_envs.sh
@@ -3,13 +3,26 @@
 # conda_backup_envs.sh
 # script from https://github.com/conda/conda/issues/5165
 # author: https://github.com/DavidMertz
+# slightly modified by Stephane PLaisance - VIB Nucleomics Core
+# version 1.1; 2022-06-09
 
+# initialize conda (adapt to your own path!)
+source /etc/profile.d/conda.sh
+
+# create date-tagged folder
 NOW=$(date "+%Y-%m-%d")
-destdir=$HOME/conda_backups/envs-$NOW
+destdir=$PWD/conda_env_backups_$NOW
 mkdir -p ${destdir}
+
+# list all available conda envs
 ENVS=$(conda env list | grep '^\w' | cut -d' ' -f1)
-for env in $ENVS; do
-    source activate $env
-    conda env export > ${destdir}/$env.yml
-    echo "Exporting $env"
+
+# loop and backup to yaml definition files
+for myenv in $ENVS; do
+  conda activate ${myenv} || \
+    ( echo "# the conda environment ${myenv} was not found on this machine" ;
+      echo "# please read the top part of the script!" \
+      && exit 1 )
+      echo "Exporting ${myenv}"
+      conda env export > ${destdir}/${myenv}.yml
 done
