39a5433a1b090756ff6ad82b7e5274f711763ae6
Thomas-VIB <thomas.standaert@vib.be>
2025-03-10 11:25:09 +0100

    Added web pages to be used as a webtool

---
+++ pacbio_samplesheet_validator/index.php
@@ -0,0 +1,21 @@
+<html>
+<META NAME="description" CONTENT="Validates a PacBio Samplesheet v1.0 10/03/2025">
+<META NAME="author" CONTENT="Thomas Standaert thomas.standaert@vib.be">
+<META NAME="robots" CONTENT="NOFOLLOW,NOINDEX">
+<head>
+  <title>PacBio Samplesheet Validator</title>
+  <link rel="stylesheet" type="text/css" href="http://gbw-s-nuc04.luna.kuleuven.be:8080/style_template.css">
+  <img class="logo" src="http://gbw-s-nuc04.luna.kuleuven.be:8080/images/logo.png" alt="logo">
+</head>
+<body>
+<div class="container">
+  <h1>PacBio Samplesheet Validator</h1>
+  <form method="post"action="results.php" enctype="multipart/form-data">  
+    <label>PacBio CSV:</label><input type="file" name="data[indexfile]" id="indexfile"><br>
+    <input type="submit" name="submit" value="Submit";>
+  </form>
+
+    <p><a href=http://gbw-s-nuc04.luna.kuleuven.be:8080/webtools/Workflow.htm/><< Return to Webtools</a></p>
+</div>
+</body>
+</html>
---
+++ pacbio_samplesheet_validator/results.php
@@ -0,0 +1,38 @@
+<?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+      $excelfile = rtrim($_FILES['data']['name']['indexfile']);   
+      $target_dir = "uploads/";
+      $target_file = $target_dir . $excelfile;
+      if (move_uploaded_file($_FILES['data']['tmp_name']['indexfile'], $target_file)){ 
+        $command = "scripts/pacbio_samplesheet_validator.sh -s $target_file";
+        exec($command, $output, $return);
+        $command_string = "Command used is: " . $command . "</br></br>";
+        if ($return == 0){
+          $output_string = $command_string . "<h2>Validation results:</h2>";
+          foreach($output as $line) { $output_string .= $line . "</br>"; }
+        } else {
+          $output_string = $command_string . "An error occured during validation: </br>";
+          foreach($output as $line) { $output_string .= $line . "</br>";} 
+        }
+      } 
+    }
+?>    
+
+<html>
+ <head>
+  <title>PacBio Samplesheet Validator</title>
+  <link rel="stylesheet" type="text/css" href="http://gbw-s-nuc04.luna.kuleuven.be:8080/style_template.css">
+  <img class="logo" src="http://gbw-s-nuc04.luna.kuleuven.be:8080/images/logo.png" alt="logo">
+</head>
+<body>
+<div class="container">
+<h1>PacBio Samplesheet Validator</h1>
+<p><?php
+  print($output_string);
+  ?></p>
+<br></br>
+<p><a href=<?php echo("http://".$_SERVER["HTTP_HOST"]."/index.php")?>><< Main</a></p>
+<p><a href=http://gbw-s-nuc04.luna.kuleuven.be:8080/webtools/Workflow.htm/><< Return to Webtools</a></p>
+</div>
+</body>
+</html>
