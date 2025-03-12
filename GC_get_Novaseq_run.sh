2842761041684c1d55acb473f31b98b594cd9283
Stephane Plaisance <stephane.plaisance@vib.be>
2025-03-12 14:58:22 +0100

    Update GC_get_Novaseq_run.sh

--- GC_get_Novaseq_run.sh
+++ GC_get_Novaseq_run.sh
@@ -2,34 +2,37 @@
 
 # scripts: GC_get_Novaseq_run.sh
 # download run files from bucket
-# Stephane Plaisance (VIB-NC) 2025/03/12; v1.0
-# do not sync the Thumbnail_Images subfolder
+# Stephane Plaisance (VIB-NC) 2025/03/12; v1.1
+# option to sync the Thumbnail_Images subfolder
 #
 # visit our Git: https://github.com/Nucleomics-VIB
 
-version="1.0, 2025-03-12"
+version="1.1, 2025-03-12"
 
 # default values
 destdir="/mnt/nuc-transfer/0003_Runs/Novaseq6000"
 rundir="runs"
+sync_thumbnails=false
 
 usage='# Usage: GC_get_Novaseq_run.sh <args>
 # -d <destination folder (default : '${destdir}')
 # -R <runs_dir (obtained from "gsutil ls gs://gcpi-rvvnc/": default '${rundir}')>
 # -r <run_id (obtained from "gsutil ls gs://gcpi-rvvnc/<runs_dir>", this folder will not be synched but its content will be!)>
+# -t <sync Thumbnail_Images (default: false)>
 # -l <show the list of runs_dir currently present on the server (specify -r to get subfolders)>]
 # -h <this help>
 # script version '${version}'
 # [-h for this help]'
 
-while getopts "d:R:r:lh" opt; do
+while getopts "d:R:r:tlh" opt; do
   case $opt in
     d) destdir=${OPTARG} ;;
     R) rundir=${OPTARG} ;;
     r) runid=${OPTARG} ;;
+    t) sync_thumbnails=true ;;
     l) echo "# Runs data currently available on the bucket";
-    		gsutil ls gs://gcpi-rvvnc/${rundir:-""}/${runid:-""};
-			exit 0 ;;
+       gsutil ls gs://gcpi-rvvnc/${rundir:-""}/${runid:-""};
+       exit 0 ;;
     h) echo "${usage}" >&2; exit 0 ;;
     \?) echo "Invalid option: -${OPTARG}" >&2; exit 1 ;;
     *) echo "this command requires arguments, try -h" >&2; exit 1 ;;
@@ -61,7 +64,11 @@ mkdir -p ${destdir}/${destid}
 # get run folder
 echo -e "\n# getting run data as ${destid}"
 
-cmd="gsutil -m rsync -r -x '^.*Thumbnail_Images.*$' gs://gcpi-rvvnc/${rundir}/${runid}/${destid} ${destdir}/${destid}"
+if [ "$sync_thumbnails" = true ]; then
+  cmd="gsutil -m rsync -r gs://gcpi-rvvnc/${rundir}/${runid}/${destid} ${destdir}/${destid}"
+else
+  cmd="gsutil -m rsync -r -x '^.*Thumbnail_Images.*$' gs://gcpi-rvvnc/${rundir}/${runid}/${destid} ${destdir}/${destid}"
+fi
 
 echo "# cmd: ${cmd}"
 
